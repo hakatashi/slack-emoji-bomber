2bcce0a4604c4f68fd86a8524d629a70a4872c96
Koki Takahashi <hakatasiloving@gmail.com>
2017-09-08 17:34:08 +0900

    Remove check if the message have colon as initial letter

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(async (req, res) => {
 			return null;
 		})();
 
-		if (message && message.startsWith(':')) {
+		if (message !== null) {
 			return uniq(message.match(/:.+?:/g).map(s => s.slice(1, -1)));
 		}
 
